9f56fa3a8c372efb68fc66252f8f8faa662dd95e
APP × PY <mayseyfaynpokemon@gmail.com>
2020-01-01 01:28:11 +0300

    GUI develop

---
+++ Bot/test.py
@@ -0,0 +1,5 @@
+import urllib.request
+import requests
+prefix = requests.get('https://chromedriver.storage.googleapis.com/LATEST_RELEASE')
+link = "http://chromedriver.storage.googleapis.com/" + prefix.text + '/chromedriver_mac64.zip'
+urllib.request.urlretrieve(link, "chromedriver.zip")
